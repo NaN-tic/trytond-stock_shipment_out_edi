d0964f4f62ea67f09acd6665d9a09941d0de1e17
Jared Esparza <jared.esparza@nan-tic.com>
2021-05-31 16:06:31 +0200

    Add check of shipment origins in generate_edi

--- stock.py
+++ stock.py
@@ -3,6 +3,8 @@ from jinja2 import Template
 from trytond.model import Workflow, ModelView, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
+from trytond.exceptions import UserError
+from trytond.i18n import gettext
 
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,8 +20,6 @@ UOMS = {
 class ShipmentOut(metaclass=PoolMeta):
     __name__ = 'stock.shipment.out'
 
-    from_edi = fields.Function(fields.Boolean('From EDI?'), 'get_from_edi')
-
     @classmethod
     def __setup__(cls):
         super(ShipmentOut, cls).__setup__()
@@ -28,7 +28,17 @@ class ShipmentOut(metaclass=PoolMeta):
                 })
 
     def get_from_edi(self, name):
-        # TODO: RETURN IF THE SALE FROM ORIGIN IS CREATED FORM EDI
+        pool = Pool()
+        Sale = pool.get('sale.sale')
+
+        origins = self.origins.split(', ')
+        sales = Sale.search(['number', 'in', origins])
+
+        if not sales:
+            return False
+        for sale in sales:
+            if not sale.edi:
+                return False
         return True
 
     @classmethod
@@ -47,18 +57,27 @@ class ShipmentOut(metaclass=PoolMeta):
     def generate_edi(self):
         pool = Pool()
         StockConfiguration = pool.get('stock.configuration')
+        Sale = pool.get('sale.sale')
         config = StockConfiguration(1)
         template_name = 'shipment_out_edi_template.jinja2'
         result_name = 'shipment_{}.PLA'.format(self.number)
         template_path = os.path.join(MODULE_PATH, template_name)
         result_path = os.path.join(config.outbox_path_edi, result_name)
-
-        if self.from_edi:
-            with open(template_path) as file_:
-                template = Template(file_.read())
-            edi_file = template.render({'shipment': self, 'UOMS': UOMS})
-            with open(result_path, 'w') as f:
-                f.write(edi_file)
+        origins = self.origins.split(', ')
+        sales = Sale.search(['number', 'in', origins])
+        if len(sales) > 1:
+            for sale in sales:
+                if sale.edi:
+                    raise UserError(gettext(
+                        'stock_shipment_out.msg_no_unique_edi',
+                        number=self.number))
+        elif len(sales) == 1:
+            if sales[0].edi:
+                with open(template_path) as file_:
+                    template = Template(file_.read())
+                edi_file = template.render({'shipment': self, 'UOMS': UOMS})
+                with open(result_path, 'w') as f:
+                    f.write(edi_file)
 
 
 class StockConfiguration(metaclass=PoolMeta):
