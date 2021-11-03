8ddd9ccf80469b18e71029329d5acd11966893d8
jared-nan <60256728+jared-nan@users.noreply.github.com>
2021-11-03 16:22:40 +0100

    Add is_edi to shipment.out model and update translations | #047562

--- __init__.py
+++ __init__.py
@@ -3,12 +3,14 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 from . import stock
+from . import sale
 
 
 def register():
     Pool.register(
         stock.ShipmentOut,
         stock.StockConfiguration,
+        sale.Sale,
         module='stock_shipment_out_edi', type_='model')
     Pool.register(
         module='stock_shipment_out_edi', type_='wizard')
